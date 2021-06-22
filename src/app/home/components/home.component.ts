089e3f413abf408369b24c5e415d9b404eb9be1d
Mario Romero <marioromero959@gmail.com>
2021-06-22 12:23:44 -0300

    Eliminando modulos

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -11,8 +11,12 @@ export class HomeComponent implements OnInit {
 
   email: FormControl;
   mensaje: FormControl;
+  name: FormControl;
 
   constructor(private router: Router) {
+    this.name = new FormControl( '', [
+      Validators.required
+    ]);
     this.email = new FormControl('', [
       Validators.required,
       Validators.email
@@ -27,6 +31,7 @@ export class HomeComponent implements OnInit {
 
   emailEnviado() {
     if (this.email.valid && this.mensaje.valid){
+      console.log(this.name.value);
       console.log(this.email.value);
       console.log(this.mensaje.value);
       alert('Su mensaje ha sido enviado con exito');
