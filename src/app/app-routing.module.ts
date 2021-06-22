0ea0044997b716f8b7f590072256d7fbc50940e2
Mario Romero <marioromero959@gmail.com>
2021-06-22 13:17:29 -0300

    Actualizando nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './home/components/home.component';
 
 const routes: Routes = [
 
@@ -13,7 +12,6 @@ pathMatch: 'full',
 path: 'home',
 loadChildren: () => import('../app/home/home.module').then(m => m.HomeModule)
 },
-
 ];
 
 @NgModule({
